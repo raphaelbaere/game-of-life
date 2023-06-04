4e5db9a72b334b516b5023a45777ba20ae075e7e
raphaelbaere <raphaelbaere@id.uff.br>
2023-06-04 15:12:47 -0300

    Consegui! Acho que sim!

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ const gameBoard = document.querySelector('#game-of-life-board');
 const startGameButton = document.querySelector('#start-game-button');
 const nextGenerationButton = document.querySelector('#next-generation-button');
 
-let board = [];
-
 // Criar o tabuleiro
 
 // Função para exibir o tabuleiro
@@ -21,7 +19,6 @@ function displayBoard() {
         div.style.height = '10px';
         div.style.backgroundColor = 'green';
         div.className = 'alive';
-        board[i] = div;
         gameBoard.appendChild(div);
     } else {
         const div = document.createElement('div');
@@ -29,17 +26,18 @@ function displayBoard() {
         div.style.height = '10px';
         div.style.backgroundColor = 'white';
         div.className = 'dead';
-        board[i] = div;
         gameBoard.appendChild(div);
     }
   }
 }
 
 function nextGeneration() {
+    const childNodes = gameBoard.childNodes;
+    const childrenArray = Array.from(childNodes);
     gameBoard.innerHTML = '';
     const boardReformed = [];
-    for (let i = 0; i < board.length; i += 80) {
-      const chunk = board.slice(i, i + 80);
+    for (let i = 0; i < childrenArray.length; i += 80) {
+      const chunk = childrenArray.slice(i, i + 80);
       boardReformed.push(chunk);
     }
 
@@ -107,7 +105,6 @@ function nextGeneration() {
                 div.style.height = '10px';
                 div.style.backgroundColor = 'green';
                 div.className = 'alive';
-                board[i] = div;
                 gameBoard.appendChild(div);
               } else if (boardReformed[i][j].className === 'alive' && (numberOfAliveNeighbors < 2 || numberOfAliveNeighbors > 3)) {
                 const div = document.createElement('div');
@@ -115,14 +112,12 @@ function nextGeneration() {
                 div.style.height = '10px';
                 div.style.backgroundColor = 'white';
                 div.className = 'dead';
-                board[i] = div;
                 gameBoard.appendChild(div);
               } else {
-                board[i] = boardReformed[i][j];
                 gameBoard.appendChild(boardReformed[i][j]);
               }
         }
-    }
+    }x
 }
 
 
@@ -131,7 +126,7 @@ function runGameOfLife() {
   displayBoard();
   setInterval(() => {
     nextGeneration()
- }, 1000); // Atualizar a cada 1 segundo
+ }, 70); // Atualizar a cada 1 segundo
 }
 
 // Iniciar o jogo
